c11063ba164de99c86a9e454a1bb12002073c3fd
Mario Gonzalez <mario_mmo@hotmail.com>
2023-10-17 22:29:06 -0300

    Primer Commit

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -3,7 +3,7 @@
 
 # A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
 
-print('Hola Mundo!')
+print('Hola Mundo!' + ' ' + 'Soy Mario')
 a = 20
 while a > 0:
     print(a)
